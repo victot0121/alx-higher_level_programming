1ef2ec2d69ba7ff4f9bd85d6392ded90e33dd9e0
victor <victoroyiga01@gmail.com>
2022-07-19 10:15:38 +0100

    commit update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    counter = 0
-    for idx in range(x):
-        try:
-            print(f'{my_list[idx]}', end='')
+"""Empty Class Square"""
 
-        except (IndexError):
-            continue
-        
-        counter += 1
-    print()
-    return (counter)
-    
+
+class Square:
+    """Empty Class Square"""
+    pass
